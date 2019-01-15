5f00cb685b54d7548884936baa224e1147f46ed7
Chris Kim <christopherjkim@gmail.com>
2019-01-14 19:03:46 -0500

    rope extending/retracting with up/down keys

--- src/player.ts
+++ src/player.ts
@@ -1,7 +1,17 @@
+import Phaser from "phaser";
 import { TestScene } from "./scenes/test.scene";
+
+interface IRopeSegment extends Phaser.Physics.Matter.Sprite {
+  joint?: MatterJS.Constraint;
+}
 
 export class Player extends Phaser.Physics.Matter.Sprite {
   public scene!: TestScene;
+
+  public ropeSegments: IRopeSegment[];
+
+  public retractRopeTimer: Phaser.Time.TimerEvent;
+  public extendRopeTimer: Phaser.Time.TimerEvent;
 
   constructor(
     world: Phaser.Physics.Matter.World,
@@ -17,8 +27,26 @@ export class Player extends Phaser.Physics.Matter.Sprite {
       // https://github.com/photonstorm/phaser/blob/v3.14.0/src/physics/matter-js/lib/body/Body.js
 
       // Infinity prevents rotation after collision (default 0)
+      // can also be done with this.setFixedRotation()
       inertia: Infinity,
     });
+
+    this.extendRopeTimer = this.scene.time.addEvent({
+      delay: 100, // ms
+      callback: () => { this.addRopeSegment(); },
+      loop: true,
+      paused: true,
+    });
+
+    this.retractRopeTimer = this.scene.time.addEvent({
+      delay: 130, // ms
+      callback: () => { this.removeRopeSegment(); },
+      loop: true,
+      paused: true,
+    });
+
+    this.ropeSegments = [];
+    this.addRopeSegment();
   }
 
   public update(time: number, delta: number): void {
@@ -28,10 +56,88 @@ export class Player extends Phaser.Physics.Matter.Sprite {
       this.setVelocityX(5);
     }
 
-    if (this.scene.cursors.up!.isDown) {
-      this.setVelocityY(-5);
-    } else if (this.scene.cursors.down!.isDown) {
-      this.setVelocityY(5);
+    if (this.scene.cursors.down!.isUp) {
+      this.retractRopeTimer.paused = true;
+    }
+    if (this.scene.cursors.up!.isUp) {
+      this.extendRopeTimer.paused = true;
+    }
+
+    if (this.scene.cursors.down!.isDown) {
+      this.extendRope();
+    } else if (this.scene.cursors.up!.isDown) {
+      this.retractRope();
+    }
+
+    const ropeHead = this.getRopeHead();
+    if (ropeHead) {
+      ropeHead.setX(this.x);
+      ropeHead.setY(this.y + 50);
+    }
+  }
+
+  private extendRope(): void {
+    if (this.extendRopeTimer.paused) {
+      this.extendRopeTimer.paused = false;
+    }
+  }
+  private retractRope(): void {
+    if (this.retractRopeTimer.paused) {
+      this.retractRopeTimer.paused = false;
+    }
+  }
+
+  private getRopeHead() {
+    return this.ropeSegments[this.ropeSegments.length - 1];
+  }
+
+  private addRopeSegment(): IRopeSegment {
+    const head = this.getRopeHead();
+    const [x, y] = head ? [head.x, head.y] : [this.x, this.y + 50];
+
+    const segment = (this.scene.add.rectangle(x, y, 6, 14, 0x00aa00) as any as IRopeSegment);
+    this.scene.matter.add.gameObject(segment, { mass: 0.6, frictionAir: 0.2 });
+    segment.setCollisionCategory(this.scene.RopeCollisionCategory);
+    segment.setCollidesWith([
+      this.scene.BoundsCollisionCategory,
+      // this.scene.RopeCollisionCategory,
+    ]);
+
+    segment.setStatic(true);
+    this.ropeSegments.push(segment);
+    if (head) {
+      head.setStatic(false);
+      head.setY(head.y + 12);
+
+      const joint = this.scene.matter.add.joint(segment, head, 8, 0.6, {
+        pointA: {x: 0, y: 4},
+        pointB: {x: 0, y: -4},
+        render: {
+          anchors: false,
+          lineWidth: 1,
+          type: "line",
+        },
+      });
+      segment.joint = joint;
+    }
+    return segment;
+  }
+
+  private removeRopeSegment(): void {
+    const head = this.getRopeHead();
+    if (!head) { return; }
+
+    if (head.joint) {
+      this.scene.matter.world.removeConstraint(head.joint, false);
+      head.joint = undefined;
+    }
+    head.destroy(true);
+    this.ropeSegments.splice(this.ropeSegments.length - 1, 1);
+
+    const nextHead = this.getRopeHead();
+    if (nextHead) {
+      nextHead.setAngle(0);
+      nextHead.setStatic(true);
     }
   }
 }
--- src/scenes/test.scene.ts
+++ src/scenes/test.scene.ts
@@ -4,6 +4,11 @@ export class TestScene extends Phaser.Scene {
   public cursors!: Phaser.Input.Keyboard.CursorKeys;
   public player!: Player;
 
+  public BoundsCollisionCategory!: number;
+  public PlayerCollisionCategory!: number;
+  public PlatformCollisionCategory!: number;
+  public RopeCollisionCategory!: number;
+
   constructor() {
     super({key: "Test"});
   }
@@ -12,6 +17,11 @@ export class TestScene extends Phaser.Scene {
 
   public create() {
     this.cursors = this.input.keyboard.createCursorKeys();
+
+    this.BoundsCollisionCategory = 1;
+    this.PlayerCollisionCategory = this.matter.world.nextCategory();
+    this.PlatformCollisionCategory = this.matter.world.nextCategory();
+    this.RopeCollisionCategory = this.matter.world.nextCategory();
 
     this.matter.world.setBounds(
       0,
@@ -25,12 +35,18 @@ export class TestScene extends Phaser.Scene {
     const graphics = this.add.graphics({ x: 4, y: 4, fillStyle: { color: 0x0000aa } });
     graphics.fillRectShape(new Phaser.Geom.Rectangle(0, 0, 4, 4));
 
-    const platform = this.add.rectangle(360, 80, 700, 10, 0xaa0000);
+    const platform = (this.add.rectangle(360, 80, 700, 10, 0xaa0000) as any as Phaser.Physics.Matter.Image);
     this.matter.add.gameObject(platform, { isStatic: true });
+    platform.setCollisionCategory(this.PlatformCollisionCategory);
 
-    this.player = new Player(this.matter.world, 360, 20);
+    this.player = new Player(this.matter.world, 360, 40);
     this.sys.displayList.add(this.player);
     this.sys.updateList.add(this.player);
+    this.player.setCollisionCategory(this.PlayerCollisionCategory);
+
+    this.player.setCollidesWith([this.BoundsCollisionCategory, this.PlatformCollisionCategory]);
+
+    this.matter.add.mouseSpring({});
   }
 
   public update(time: number, delta: number): void {
